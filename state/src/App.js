4f71fe56b8bb618c77d5ed93f39f80891d18e654
jungbal <terykid@naver.com>
2022-07-15 17:05:32 +0900

    react state 실습

--- state/src/App.js
+++ state/src/App.js
@@ -10,7 +10,7 @@ function CheckboxExample(){
     return(
         <div className="App">
             <input type="checkbox" checked={isChecked} onChange={handleChecked} />
-            <span>{isChecked ? "Checked!!" : "Unchecked"}</span>
+            <span>{isChecked ? "Checked!!" : "Unchecked"}</span>{/*삼항 연산자로 체크박스 체크시 출력문 지정*/}
         </div>
     );
 }
